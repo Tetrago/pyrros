27cff942bf8db2ebf01b926054ed0cb884e7047c
James <jamestgeiss@gmail.com>
2023-01-31 21:30:39 -0500

    Initial project

---
+++ src/main/java/tetrago/pyrros/Pyrros.java
@@ -0,0 +1,16 @@
+package tetrago.pyrros;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Pyrros.MODID)
+public class Pyrros
+{
+    public static final String MODID = "pyrros";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Pyrros()
+    {
+    }
+}
